3f45c30bd8d38f68e05fb83bfb11aa4c74843897
Dominik Sprohar <dominik.sprohar@gmail.com>
2024-08-23 14:51:09 +0200

    basic structure and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ContactsHomeComponent } from './modules/contacts/components/contacts-home/contacts-home.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'contacts', component: ContactsHomeComponent }
+];
